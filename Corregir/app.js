fe964bcf78419d8cffa9b416cd8a1969abb4b3c6
guiyee <guillermodinanno@gmail.com>
2023-01-19 10:38:44 -0300

    corrigiendo proyecto

--- Corregir/app.js
+++ Corregir/app.js
@@ -1,80 +1,133 @@
 const packViajes = [
-    { nombre: "Harina", precio: 50 },
-    { nombre: "Galletitas", precio: 100 },
-    { nombre: "Cerveza", precio: 150 },
-    { nombre: "Leche", precio: 200 },
-    { nombre: "Gaseosa", precio: 250 },
+    {
+        city: "Sydney",
+        precio: 1200,
+        servicio: {
+            atencion: "incluye atencion al cliente 24/7",
+            seguro: "seguro premium", precio: 100,
+            seguro: "seguro low cost", precio: 50,
+            equipaje: "equipaje premium", precio: 50,
+            equipaje: "equipaje low cost", precio: 25,
+            plan: "planificacion premium", precio: 75,
+            plan: "planificacion low cost", precio: 40
+        }
+    },
+    {
+        city: "Melbourne",
+        precio: 1100,
+        servicio: {
+            atencion: "incluye atencion al cliente 24/7",
+            seguro: "seguro premium", precio: 100,
+            seguro: "seguro low cost", precio: 50,
+            equipaje: "equipaje premium", precio: 50,
+            equipaje: "equipaje low cost", precio: 25,
+            plan: "planificacion premium", precio: 75,
+            plan: "planificacion low cost", precio: 40
+        }
+    },
+    {
+        city: "Brisbane",
+        precio: 1000,
+        servicio: {
+            atencion: "incluye atencion al cliente 24/7",
+            seguro: "seguro premium", precio: 100,
+            seguro: "seguro low cost", precio: 50,
+            equipaje: "equipaje premium", precio: 50,
+            equipaje: "equipaje low cost", precio: 25,
+            plan: "planificacion premium", precio: 75,
+            plan: "planificacion low cost", precio: 40
+        }
+    },
+    {
+        city: "Gold Coast",
+        precio: 950,
+        servicio: {
+            atencion: "incluye atencion al cliente 24/7",
+            seguro: "seguro premium", precio: 100,
+            seguro: "seguro low cost", precio: 50,
+            equipaje: "equipaje premium", precio: 50,
+            equipaje: "equipaje low cost", precio: 25,
+            plan: "planificacion premium", precio: 75,
+            plan: "planificacion low cost", precio: 40
+        }
+    },
 ];
-let carrito = [];
 
-let destino = prompt("Desea elegir un destino de viaje?  Si o No");
+const carrito = [];
+
+let destino = prompt("¿Desea seleccionar un destino de viaje? Si o No")
 
 while (destino != "si" && destino != "no") {
-    alert("Por favor ingrese Si o No")
-    destino = prompt("Desea elegir un destino de viaje?  Si o No ")
+    alert("Respuesta invalida")
+    destino = prompt("Por favor escriba Si o No")
 }
+
+
 
 if (destino == "si") {
-    alert("A continuacion nuestra lista de destinos posibles")
+    alert("Estos son los destinos y precios. Incluye servicios para seleccionar")
+    let eleccionUsuario = packViajes.map((elDestino) => elDestino.city + " " + "$" + elDestino.precio);
+    alert(eleccionUsuario.join(" - "))
 
 } else if (destino == "no") {
-    alert("Vayase a cagar entonces!")
-}
-
-while (destino != "no") {
-    let viaje = prompt("Elija su destino  \n\n Sydney $1200 \n Melbourne $1100 \n Brisbane $1000 \n Gold Coast $950 \n\n Salir")
-    
-    
-
-    if (viaje == "sydney" || viaje == "melbourne" || viaje == "brisbane" || viaje == "gold coast") {
-       let productoEncontrado = packViajes.find((pack) => pack.nombre == viaje)
-
-    } else if (viaje == "salir") {
-        alert("Usted se lo pierde")
-        break
-    } else {
-       viaje = destino
-    }
-
-
-    let unidades = parseInt(prompt("Cuantos boletos desea comprar?"))
-
-    carrito.push({ viaje, unidades, precio })
-
-
-    /* } else if (viaje == "salir") {
-        alert("Usted se lo pierde")
-        break
-    } else {
-        destino
-    } */
-
-    destino = prompt("Desea seguir eligiendo?  Si o No")
-
-    while (destino != "si" && destino != "no") {
-        alert("Por favor ingrese Si o No")
-        destino = prompt("Desea comprar algun viaje?  Si o No ")
-    }
-
-    if (destino == "si") {
-        alert("Puede seguir seleccionando")
-
-    } else if (destino == "no") {
-        alert("Muchas gracias por su compra")
-    }
-
-
-    while (destino === "no") {
-        carrito.forEach((carritoFinal) => {
-            alert(` Destino: ${carritoFinal.viaje} \n Unidades: ${carritoFinal.unidades} \n Total a pagar: ${carritoFinal.unidades * carritoFinal.precio}`)
-        })
-        break
-    }
-
+    alert("Hasta pronto")
 
 }
 
-const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0)
-alert(`El total a pagar de los viajes y servicios es: ${total}`)
+
+while (destino == "si") {
+
+    let eleccionUsuario = prompt("Agrega un destino a tu carrito")
+
+    let elijaDestino = packViajes.find((viaje) => viaje.city == eleccionUsuario)
+
+    if (elijaDestino != undefined) {
+
+        let unidades = parseInt(prompt("Cuantos pasajes desea comprar?"))
+        
+        carrito.push({ Ciudad: eleccionUsuario, Precio: elijaDestino.precio, Cantidad: unidades })
+
+        if(unidades <=20  || !isNaN (unidades)){
+            alert("ok")
+
+        } else {
+            alert("opcion invalida")
+            unidades = parseInt(prompt("Cuantos pasajes desea comprar?"))
+        }
+
+    } else {
+        alert("No tenemos esos destinos")
+    }
+
+    do {
+
+        destino = prompt("Desea seguir comprando? Si o No")
+
+        let pregunta = destino;
+
+        if (pregunta === "no") {
+
+            alert("Gracias por su compra. Adios!")
+            carrito.forEach((carritoFinal) => {
+                alert(`Destino: ${carritoFinal.Ciudad}, Precio: ${carritoFinal.Precio}, Pasajes: ${carritoFinal.Cantidad} `)
+            })
+
+        } else if (pregunta === "si") {
+            alert("Siga comprando")
+
+        } else {
+            alert("Opcion invalida")
+        }
+
+    } while (destino != "si" && destino != "no")
+
+}
+
+const total = carrito.reduce((acumulador, numero) => acumulador + numero.Precio * numero.Cantidad, 0)
+alert(`El total a pagar es: ${total}`)
+
+
+
+
 
 
