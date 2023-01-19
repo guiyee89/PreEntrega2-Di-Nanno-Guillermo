df8e60cd592766ed72cc7ae1e8f631db45f2e644
guiyee <guillermodinanno@gmail.com>
2023-01-19 20:04:28 -0300

    Termine

---
+++ app.js
@@ -0,0 +1,101 @@
+
+const carrito = [];
+let eleccionUsuario
+let unidades
+let carritoFinal
+// PREGUNTA INICIAL
+function init() {
+    let destino
+    destino = prompt("¿Desea seleccionar un destino de viaje? Si o No").toLowerCase();
+
+    if (destino === "si") {
+        alert("Estos son los destinos y precios. Incluye servicios para seleccionar")
+        elegirDestino();
+
+    } else if (destino === "no") {
+        alert("Hasta pronto")
+
+    } else {
+        alert("Respuesta invalida. Por favor escriba Si o No")
+        init()
+    }
+
+}
+init()
+
+// USUARIO ELIGE DESTINO
+
+function elegirDestino() {
+
+    let eleccionDestino = packViajes.map((elDestino) => elDestino.city + " " + "$" + elDestino.precio);
+    alert(eleccionDestino.join(" - "))
+
+    eleccionDestino = prompt("A que ciudad desea viajar?")/* .toLowerCase() no funciona aca */
+
+    eleccionUsuario = packViajes.find((viaje) => viaje.city == eleccionDestino);
+
+    if (eleccionUsuario != undefined) {
+        seleccionarPasajes()
+
+    } else {
+        alert("Opcion Invalida. Escriba una de las ciudades del catalogo")
+        elegirDestino()
+    }
+
+}
+
+function seleccionarPasajes() {
+
+    unidades = parseInt(prompt("Cuantos pasajes desea comprar?"))
+    if (unidades >= 1 && unidades <= 10) {
+        alert("Excelente! Que disfrute su viaje!")
+        acumuladorCarrito()
+
+    } else {
+        alert("Opcion invalida. Hasta un maximo de 10 pasajes")
+        seleccionarPasajes()
+    }
+
+}
+
+function acumuladorCarrito() {
+    let seguirComprando
+
+    carrito.push({
+        Ciudad: eleccionUsuario.city,
+        Precio: eleccionUsuario.precio,
+        Cantidad: unidades,
+
+    })
+
+    do {
+        seguirComprando = prompt("Desea seguir comprando? Si o No")
+
+        if (seguirComprando === "no") {
+
+            alert("Gracias por su compra. Adios!")
+
+            carrito.forEach((carritoFinal) => {
+                alert(`Destino: ${carritoFinal.Ciudad}, Precio: ${carritoFinal.Precio}, Pasajes: ${carritoFinal.Cantidad} `)
+            })
+
+        } else if (seguirComprando == "si") {
+            alert("Puede seguir seleccionando")
+            elegirDestino()
+        } else {
+            alert("Opcion invalida")
+        }
+
+    } while (seguirComprando != "si" && seguirComprando != "no")
+
+
+}
+
+const total = carrito.reduce((acumulador, numero) => acumulador + numero.Precio * numero.Cantidad, 0)
+alert(`El total a pagar es: ${total}`)
+
+
+
+
+
+
---
+++ arrays.js
@@ -0,0 +1,54 @@
+const packViajes = [
+    {
+        city: "Sydney",
+        precio: 1200,
+        servicio: {
+            atencion: "incluye atencion al cliente 24/7", 
+            seguro: "seguro premium", precio: 100,
+            seguro: "seguro low cost", precio: 50,
+            equipaje: "equipaje premium", precio: 50,
+            equipaje: "equipaje low cost", precio: 25,
+            plan: "planificacion premium", precio: 75,
+            plan: "planificacion low cost", precio: 40
+        }
+    },
+    {
+        city: "Melbourne",
+        precio: 1100,
+        servicio: {
+            atencion: "incluye atencion al cliente 24/7",
+            seguro: "seguro premium", precio: 100,
+            seguro: "seguro low cost", precio: 50,
+            equipaje: "equipaje premium", precio: 50,
+            equipaje: "equipaje low cost", precio: 25,
+            plan: "planificacion premium", precio: 75,
+            plan: "planificacion low cost", precio: 40
+        }
+    },
+    {
+        city: "Brisbane",
+        precio: 1000,
+        servicio: {
+            atencion: "incluye atencion al cliente 24/7",
+            seguro: "seguro premium", precio: 100,
+            seguro: "seguro low cost", precio: 50,
+            equipaje: "equipaje premium", precio: 50,
+            equipaje: "equipaje low cost", precio: 25,
+            plan: "planificacion premium", precio: 75,
+            plan: "planificacion low cost", precio: 40
+        }
+    },
+    {
+        city: "Gold Coast",
+        precio: 950,
+        servicio: {
+            atencion: "incluye atencion al cliente 24/7",
+            seguro: "seguro premium", precio: 100,
+            seguro: "seguro low cost", precio: 50,
+            equipaje: "equipaje premium", precio: 50,
+            equipaje: "equipaje low cost", precio: 25,
+            plan: "planificacion premium", precio: 75,
+            plan: "planificacion low cost", precio: 40
+        }
+    },
+];
